813b14871efcee364f87cb242fc759d805fa3af2
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-12-09 19:01:35 +0530

    drop: use atomics

--- tut/drop.erl
+++ tut/drop.erl
@@ -1,8 +1,7 @@
 -module(drop).
--export([fall_velocity/1, mps_to_mph/1, mps_to_kph/1]).
+-export([fall_velocity/2]).
 
-fall_velocity(Distance) -> math:sqrt(2 * 9.8 * Distance).
+fall_velocity(earth, Distance) -> math:sqrt(2 * 9.8 * Distance);
+fall_velocity(moon, Distance) -> math:sqrt(2 * 1.6 * Distance);
+fall_velocity(mars, Distance) -> math:sqrt(2 * 3.71 * Distance).
 
-mps_to_mph(Mps) -> 2.23693629 * Mps.
-
-mps_to_kph(Mps) -> 3.6 * Mps.
