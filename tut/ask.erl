e77e475c5cf055d942cbad56fc55219383a93627
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-12-09 19:43:22 +0530

    ask: read input from user

---
+++ tut/ask.erl
@@ -0,0 +1,22 @@
+-module(ask).
+-export([term/0]).
+
+term() ->
+	Input = io:read("What {planemo, distance} ? >>"),
+	process_term(Input).
+
+process_term({ok, Term}) when is_tuple(Term) ->
+	Velocity = drop:fall_velocity(Term),
+	io:format("Yeilds ~w. ~n", [Velocity]),
+	term();
+
+process_term({ok, quit}) ->
+	io:format("Goodbye.~n");
+
+process_term({ok, _}) ->
+	io:format("You must enter a tuple.~n"),
+	term();
+
+process_term({error, _}) ->
+	io:format("You must enter a tuple with correct syntax.~n"),
+	term().
