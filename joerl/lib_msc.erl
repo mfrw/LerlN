e6f6e77c690dbd71cf24e62030b3605af9bce409
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-12-16 19:19:36 +0530

    lib: acculators example

--- joerl/lib_msc.erl
+++ joerl/lib_msc.erl
@@ -23,3 +23,24 @@ pythagtriplet(N) ->
 % Permutations
 perms([]) -> [[]];
 perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+% Maximum using guards
+max(X, Y) when X > Y -> X;
+max(X, Y) -> Y.
+
+% Bad Code!
+odds_and_evens(L) ->
+	Odds = [X || X <- L, (X rem 2) =:= 1],
+	Evens = [X || X <- L, (X rem 2) =:= 0],
+	{Odds, Evens}.
+
+% Accumulators
+odds_and_evens_acc(L) ->
+	odds_and_evens_acc(L, [], []).
+odds_and_evens_acc([H|T], Odds, Evens) ->
+	case (H rem 2) of
+		1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+		0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+	end;
+odds_and_evens_acc([], Odds, Evens) ->
+	{Odds, Evens}.
